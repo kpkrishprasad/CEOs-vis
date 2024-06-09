ab85adcd987d42021046fcb12f5a53d75b8a7039
Krish Prasad <krishprasad@Krishs-MacBook-Pro.local>
2024-06-09 00:20:27 -0700

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -21,5 +21,5 @@ export default defineConfig({
       '@': '/src',  // This allows you to use '@/somePath' as an alias to '/src/somePath'
     }
   },
-  base: '/vite-deploy-demo/'
+  base: 'CEOs-HATE-This-Simple-Trick/'
 });
