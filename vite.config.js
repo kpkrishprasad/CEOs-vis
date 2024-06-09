18e3b34c01ab436ed7b9128a668903cbce40fa9d
Krish Prasad <krishprasad@Krishs-MacBook-Pro.local>
2024-06-08 22:42:12 -0700

    a

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-+  base: '/vite-deploy-demo/'
-})
-
-
-
+  plugins: [svelte()],
+  base: '/CEOs-HATE-This-Simple-Trick/',  // Adjust this if deploying to a subpath, use './' or '/' if at the root
+  build: {
+    outDir: 'dist',  // Customize the output directory
+    rollupOptions: {
+      output: {
+        // Ensures that assets are placed in a structured way if necessary
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'build/[name].[hash].js',
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',  // This allows you to use '@/somePath' as an alias to '/src/somePath'
+    }
+  }
+});
